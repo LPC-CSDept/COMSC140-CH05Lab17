6f33ed066ef86c4e344a9302711071689fc19318
Kyuwoong Lee <kwooler@gmail.com>
2022-10-23 22:14:38 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+
+[Link to Chap 5 Lab16](https://docs.google.com/presentation/d/1r3h2R9JwK9HK_U2Ia-zncL0BSjHV6Giu6ugNJ6yZpgc/edit#slide=id.g1204f84631c_0_237)
+
+![Lab 16](https://nimbus-screenshots.s3.amazonaws.com/s/4c8532db68e12cd64f93542c34879633.png)
+
+## Compete the "main.py"
+
+
---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+##################################################
+# Make your lambda function here
+# greater =
+# filter50 =
+##################################################
+
+
+print(greater(10, 20))
+print(greater(20, 10))
+print(greater(100, 20))
+
+numbers = [random.randint(0, 100) for i in range(10)]
+print('original list', numbers)
+print('filter 50', filter50(numbers))
